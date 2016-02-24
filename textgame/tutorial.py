b4a46545f28c8549e659321c998765117267d7c3
Kevin Cole <msirabel@gmail.com>
2016-02-24 12:49:18 -0500

    - fixed script closing without welcoming warrior

--- textgame/tutorial.py
+++ textgame/tutorial.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 print("design character")
 
+
+
 Name = input("what is your characters name: ")
 print("hello", Name)
 
 Class = input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
-if Class == 1:
+if Class =="1":
     print("welcome Warrior")
+
