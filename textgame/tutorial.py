38b64f26b9505bd5f403451456ee7b7da0dec901
Kevin Cole <msirabel@gmail.com>
2016-02-24 10:58:42 -0500

    - Added launch program to tell OS file type

--- textgame/tutorial.py
+++ textgame/tutorial.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python2
 print("design character")
 
 Name = input("what is your characters name: ")
 print("hello", Name)
 
-Class = input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
-if Class == 1:
+Class == input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
+if Class = 1:
     print("welcome Warrior")
