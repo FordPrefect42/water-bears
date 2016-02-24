54413764dab8fccb98ba5ca73a67ecc8d6c549dd
Kevin Cole <msirabel@gmail.com>
2016-02-24 11:00:22 -0500

    - Fixed syntax

--- textgame/tutorial.py
+++ textgame/tutorial.py
@@ -4,6 +4,6 @@ print("design character")
 Name = input("what is your characters name: ")
 print("hello", Name)
 
-Class == input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
-if Class = 1:
+Class = input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
+if Class == 1:
     print("welcome Warrior")
