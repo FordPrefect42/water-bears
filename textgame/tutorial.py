48c9ebfa64574753a753eda40aa04235e851e1f6
FordPrefect42 <Yaakovred@gmail.com>
2016-02-25 12:09:47 -0500

    - welcoming for each class

--- textgame/tutorial.py
+++ textgame/tutorial.py
@@ -7,6 +7,12 @@ Name = input("what is your characters name: ")
 print("hello", Name)
 
 Class = input("choose your class: (1) Warrior, (2) Mage, (3) Theif, or (4) Hunter:")
-if Class =="1":
+if Class == "1":
     print("welcome Warrior")
+if Class == "2":
+    print("welcome Mage")
+if Class == "3":
+    print("welcome Theif")
+if Class == "4":
+    print("welcome Hunter")
 
