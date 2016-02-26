1bf551d9eda0d61e6b032a2603096a4c85db453e
Marco Sirabella <msirabel@gmail.com>
2016-02-26 17:32:35 -0500

    - Fixed fullscreen, res at 16x9

--- graphicgame/tutorial.py
+++ graphicgame/tutorial.py
@@ -1,5 +1,14 @@
-
 #!/usr/bin/env python3
 
-from pygame import *
+import sys
+import pygame
+
+pygame.init()
+size = width, height = 16, 9
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode((size), pygame. FULLSCREEN)
+
+input("wait")
 
