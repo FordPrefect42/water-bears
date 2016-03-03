5c12f687be655fecbe59a1726fff3aa93d8d77be
Marco Sirabella <msirabel@gmail.com>
2016-03-03 17:29:15 -0500

    - Fixed restest.py to only display resolution and nothing else except a   0 on the second line

--- src/graphicgame/restest.py
+++ src/graphicgame/restest.py
@@ -4,8 +4,6 @@ import sys
 import pygame
 import os
 
-resolution = os.system("xrandr | grep '*'")
+resolution = os.system("xrandr | grep '*' | cut -d ' ' -f 4 | head -n 1")
 print(resolution)
-
-input("wait")
 
