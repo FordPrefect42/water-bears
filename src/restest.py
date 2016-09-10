0d3074c8f341804e6bf613c14c9877bbda1a7c9a
Marco Sirabella <msirabel@gmail.com>
2016-09-10 15:13:53 -0400

    - Working screen tester **HAVE YET TO TEST MUCH**

--- src/restest.py
+++ src/restest.py
@@ -2,8 +2,13 @@
 
 import sys
 import pygame
-import os
 
-resolution = os.system("xrandr | grep '*' | cut -d ' ' -f 4 | head -n 1")
+#resolution = os.system("xrandr | grep '*' | cut -d ' ' -f 4 | head -n 1")
+pygame.init()
+resolution = pygame.display.Info()
+pygame.quit()
+#res = resolution
+#size = width, height = (res.current_w, res.current_h)
 print(resolution)
 
+sys.exit(0)
