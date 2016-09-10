479f311fc58aba8dcad7f2c694c2aac2fb9205b8
Marco Sirabella <msirabel@gmail.com>
2016-09-10 15:45:03 -0400

    - Added file to put in sprite classes

---
+++ src/sprites.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pygame
+
+__appname__    = "Water Bears"
+__author__     = "Marco Sirabella"
+__copyright__  = ""
+__credits__    = "Marco Sirabella, Christopher Adams, Foo Bar"
+__license__    = ""
+__version__    = "0.1.0"
+__maintainers__= "Marco Sirabella, Christopher Adams"
+__email__      = "msirabel@gmail.com"
+__status__     = "Prototype"
+__module__     = ""
+
+"""
+This is where all sprites to be imported go
+might be made into a package or module later on to save time
+and organize
+"""
