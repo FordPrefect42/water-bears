8d9b6fe7015bf0eaeba4290fdded391dbac2ab01
Marco Sirabella <msirabel@gmail.com>
2016-09-16 15:33:01 -0400

    - Renamed variables

---
+++ src/variables.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pygame
+
+__appname__    = "Water Bears"
+__author__     = "Marco Sirabella"
+__copyright__  = ""
+__credits__    = "Marco Sirabella, Christopher Adams"
+__license__    = ""
+__version__    = "0.0.1"
+__maintainers__= "Marco Sirabella, Christopher Adams"
+__email__      = "msirabel@gmail.com"
+__status__     = "Prototype"
+__module__     = ""
+
+speed = [2, 2]
+black = 0, 0, 0
+yellow = 255, 255, 0
+orange = 255, 255/2, 0
+blue = 0, 0, 255
+fullscreen = True
